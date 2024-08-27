c850073bfe9cc5bb5d5e3fb6a15ae24b1c2113a0
Mohamed Elshfae <mohamedelshafae888@gmail.com>
2024-08-28 01:04:31 +0300

    add identity authentication

---
+++ FluffyApp/FluffyApp.EF/Data/ChatContext.cs
@@ -0,0 +1,36 @@
+﻿
+using FluffyApp.Core.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluffyApp.EF.Data
+{
+    public class ChatContext: IdentityDbContext<User ,IdentityRole<Guid>, Guid>
+    {
+        public DbSet<User> Users {  get; set; }
+        public ChatContext(DbContextOptions<ChatContext> options) :base(options)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Entity<IdentityRole<Guid>>().ToTable("Roles");
+            modelBuilder.Entity<IdentityUserRole<Guid>>().ToTable("UserRoles");
+            modelBuilder.Entity<IdentityUserClaim<Guid>>().ToTable("UserClaims");
+            modelBuilder.Entity<IdentityUserLogin<Guid>>().ToTable("UserLogins");
+            modelBuilder.Entity<IdentityRoleClaim<Guid>>().ToTable("RoleClaims");
+            modelBuilder.Entity<IdentityUserToken<Guid>>().ToTable("UserTokens");
+        }
+
+
+    }
+}
--- FluffyApp/FluffyApp/Program.cs
+++ FluffyApp/FluffyApp/Program.cs
@@ -1,7 +1,26 @@
+using FluffyApp.Core.Models;
+using FluffyApp.EF.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add DbContext with SQL
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
+builder.Services.AddDbContext<ChatContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddAuthentication().AddBearerToken(IdentityConstants.BearerScheme);
+builder.Services.AddAuthorization();
+
+// Configure Identity
+builder.Services.AddIdentity<User, IdentityRole<Guid>>()
+    .AddEntityFrameworkStores<ChatContext>()
+    .AddApiEndpoints();
+
+
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -15,10 +34,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.MapIdentityApi<User>();
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
